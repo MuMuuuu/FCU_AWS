1964702e3a4df06b5fff61231b41af2e6629f43e
kaoyihao <39653799+kaoyihao@users.noreply.github.com>
2021-11-29 23:08:03 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<head>
+	<title>
+		Cloud Application Team 5
+	</title>
+	<link  href="login.css" type="text/css" rel="stylesheet" >
+	<meta name="viewport" content="width=device-width , initial-scale=1.0" />
+</head>
+	
+	
+<body>
+	<input type="button" class="button1" onclick="location.href='user login.php'" value="user login" ><br><br><br><br><br>
+	<input type="button" class="button2" onclick="location.href='store login.php'" value="store/enterprise login" >
+</body>
