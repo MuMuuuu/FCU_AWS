72f385209e3391732d6bf8b15fd1bc5b49d9414e
kaoyihao <39653799+kaoyihao@users.noreply.github.com>
2021-11-29 23:48:52 +0800

    Add files via upload

---
+++ user login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<title>
+		Cloud Application Team 5
+	</title>
+	<link  href="user login.css" type="text/css" rel="stylesheet" >
+	<meta name="viewport" content="width=device-width , initial-scale=1.0" />
+</head>
+	
+<body>
+		
+	<form action="user login_res.php"  method="post">
+			<td>姓名</td><td><input type=text name="name"></td>
+		<br><td>帳號</td><td><input type=text name="id"></td>
+		<br><td>密碼</td><td><input type=password name="pwd"></td>
+		<br><br><td><input type= submit class="b1" value="送出">
+				<input type= reset  class="b2" value="取消"></td>
+</body>
